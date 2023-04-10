649326317484ae3992790e71b009e9709ffe1d3e
Juan Pablo <106168706+Jpzinn654@users.noreply.github.com>
2023-04-10 13:10:50 -0300

    Refatoração de códigos no componente de card

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
+import { Swipeable } from 'react-native-gesture-handler'
+import { Ionicons, MaterialIcons } from '@expo/vector-icons'
+
 export default function Card() {
     const [isActive, setIsActive] = useState(false);
 
@@ -8,20 +11,46 @@ export default function Card() {
         setIsActive(!isActive);
     };
 
-    return (
-        <TouchableOpacity style={styles.card} onPress={toggleCard}>
-            <View style={styles.upContainer}>
-                <Text style={styles.title}>Título do Card</Text>
-                <Text style={styles.value}>R$ 0,00</Text>
+    const leftSwipe = () => {
+        return (
+            <View style={styles.leftView}>
+                <TouchableOpacity style={styles.left}>
+                    <Ionicons name="ios-trash-outline" size={24} color="white" />
+                </TouchableOpacity>
             </View>
+        )
+    }
+
+    const rightSwipe = () => {
+        return (
+            <View style={styles.rightView}>
+                <TouchableOpacity style={styles.right}>
+                    <MaterialIcons name="edit" size={24} color="white" />
+                </TouchableOpacity>
+
+            </View>
+        )
+    }
+
+
+    return (
+
+        <TouchableOpacity style={styles.card} onPress={toggleCard}>
+            <Swipeable renderLeftActions={leftSwipe} renderRightActions={rightSwipe}>
+                <View style={styles.upContainer}>
+                    <Text style={styles.title}>Título do Card</Text>
+                    <Text style={styles.value}>R$ 0,00</Text>
+                </View>
+            </Swipeable>
             {isActive && (
                 <View style={styles.cardContent}>
                     <Text style={styles.text}>
-                        Texto do card que será exibido após o clique. 
+                        Texto do card que será exibido após o clique.
                     </Text>
                 </View>
             )}
         </TouchableOpacity>
+
     );
 }
 
@@ -41,11 +70,28 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         width: 320,
         marginTop: 10,
-
+    },
+    leftView: {
+        backgroundColor: 'red',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+    left: {
+        padding: 5,
+    },
+    rightView: {
+        backgroundColor: 'blue',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+    right: {
+        padding: 5,
     },
     upContainer: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
+        justifyContent: 'space-around',
     },
     title: {
         fontSize: 16,
